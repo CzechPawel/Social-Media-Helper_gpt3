cede55daac3a3ee5ad86daa39db7ff65056fbec0
Mathias Asberg <m@nextgrid.ai>
2022-04-06 12:37:37 +0200

    sync

---
+++ app1.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+
+def app():
+    # Setting up the Title
+    st.title("Welcome 📩")
+    st.write(
+        f"""
+        ### Showcases created by [NewNative](https://newnative.ai) by [Mathias Åsberg](https://github.com/Mindgames)
+        """
+    )
+
+    # st.header("🧠 Powered by Kairos Data Labs") 🎈 🐾 🏆
+
+    # st.write(intro)
+
+    st.write(
+        f"""
+            ---
+            ### 🔗 **Connect With Us**
+            -Visit [NewNative.ai](https://newnative.ai/)  
+           
+        """
+    )
---
+++ gpt_app.py
@@ -0,0 +1,26 @@
+import app1
+import app2
+import app3
+import app4
+import app5
+import streamlit as st
+
+st.set_page_config(page_title="GPT-3 Sandbox", page_icon="🐾", layout="centered")
+
+# Pages as key-value pairs
+PAGES = {
+    "Dashboard": app1,
+    "Summarization": app2,
+    "Q&A": app3,
+    "Explain": app4,
+    "Notes": app5,
+    # "GPT-3 Sandbox": app3,
+}
+
+st.sidebar.title("Go to:")
+
+selection = st.sidebar.radio("", list(PAGES.keys()))
+
+page = PAGES[selection]
+
+page.app()
